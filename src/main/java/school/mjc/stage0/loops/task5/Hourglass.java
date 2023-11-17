bfb2d50f01727a04faf6746fd1f9709e45eb227f
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-17 11:57:45 +0300

    feat: complete task5 module4.2 v.5

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,9 +2,9 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
 //    public static void main(String[] args) {
-//        int height = 6;
+//        int height = 5;
     public void printHourglassOfGivenSize(int height) {
-        for (int i = 1; i <= height/2 + 1; i++){
+        for (int i = 1; i <= height/2; i++){
             for (int j = 1; j <= height; j++){
                 if (j >= i && j <= height + 1 - i) {
                     System.out.print('8');
@@ -15,7 +15,7 @@ public class Hourglass {
             }
             System.out.println();
         }
-        for (int i = height/2 + 2; i <= height; i++){
+        for (int i = height/2 + 1; i <= height; i++){
             for (int j = 1; j <= height; j++){
                 if(j <= i && j >= height + 1 - i) {
                     System.out.print('8');
