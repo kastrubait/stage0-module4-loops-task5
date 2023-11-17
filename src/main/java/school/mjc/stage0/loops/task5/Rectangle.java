af1f166e1b47269f81a58a77e970e1bd35cebc59
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-17 11:08:31 +0300

    feat: complete task5 module4.2 v.1

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -4,7 +4,11 @@ public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
         for (int i = 1; i <= length; i++){
             for (int j = 1; j <= height; j++){
-                System.out.print('8');
+                if (i == 1 || i == length || j == 1 || j == height) {
+                    System.out.print('8');
+                } else {
+                    System.out.print(' ');
+                }
             }
             System.out.println();
         }
