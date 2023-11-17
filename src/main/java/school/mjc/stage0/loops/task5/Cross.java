4483399b10e5a9e04ce94d63c8395b4b571b3e62
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-17 11:17:06 +0300

    feat: complete task5 module4.2 v.3

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -1,9 +1,8 @@
 package school.mjc.stage0.loops.task5;
 
 public class Cross {
-    public static void main(String[] args) {
-        int sideLength = 5;
-//    public void printCross(int sideLength) {
+
+    public void printCross(int sideLength) {
         for (int i = 1; i <= sideLength; i++){
             for (int j = 1; j <= sideLength; j++){
                 if(i  == sideLength/2 + 1 || j == sideLength/2 + 1) {
